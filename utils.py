7b3bcaffc7c689371f7d8dacbef16e5b2d56bc3b
UchihaMadara <krishna.kp07@gmail.com>
2020-07-02 22:17:14 +0530

    Added env.py and changed timer

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 import timeit
 
+# add logging functionality
 def timer(func):
     def wrapper(*args, **kwargs):
         start_time = timeit.default_timer()
-        func(*args, **kwargs)
+        func_val = func(*args, **kwargs)
         elapsed = timeit.default_timer() - start_time
         print('Function "{name}" took {time} seconds to complete.'.format(name=func.__name__, time=elapsed))
+        return func_val
     return wrapper
 
 
